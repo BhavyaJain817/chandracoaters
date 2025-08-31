964067d99a6492793d4f43852a84db2ea2f0ab59
BhavyaJain817 <113345825+BhavyaJain817@users.noreply.github.com>
2025-08-31 22:14:04 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1098,10 +1098,7 @@ export default function Home() {
                 <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
               </svg>
             </div>
-            <div className="absolute left-16 bg-white text-gray-800 px-3 py-2 rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
-              Visit YouTube Channel
-              <div className="absolute left-0 top-1/2 transform -translate-y-1/2 -translate-x-1/2 w-2 h-2 bg-white rotate-45"></div>
-            </div>
+          
           </a>
 
           {/* WhatsApp Button */}
